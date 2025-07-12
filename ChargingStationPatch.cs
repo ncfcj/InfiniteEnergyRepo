34476cd46530605592fc9784d126d0dd99612f90
ncfcj <ncfcj@outlook.com>
2025-07-12 17:54:17 -0300

    Initial Commit

---
+++ ChargingStationPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace RepoInfiniteEnergy;
+
+[HarmonyPatch(typeof(ChargingStation))]
+static class ChargingStationPatch
+{
+    [HarmonyPatch("Start")]
+    [HarmonyPrefix]
+    private static bool Start(ref ChargingStation __instance)
+    {
+        // Prevent the charging station from starting
+        Debug.Log((object) $"InfiniteEnergy: Setting Total Charge to a very high number");
+        __instance.chargeTotal = 1000000000;
+        
+        return true;
+    }
+}
---
+++ RepoInfiniteEnergy.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace RepoInfiniteEnergy;
+
+[BepInPlugin("NiltonCosta.RepoInfiniteEnergy", "RepoInfiniteEnergy", "1.0")]
+[BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+public class RepoInfiniteEnergy : BaseUnityPlugin
+{
+    internal static RepoInfiniteEnergy Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        gameObject.transform.parent = null;
+        gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        Logger.LogInfo($"[InfiniteEnergy] v1.0 has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+
+    private void Update()
+    {
+        // Code that runs every frame goes here
+    }
+}
